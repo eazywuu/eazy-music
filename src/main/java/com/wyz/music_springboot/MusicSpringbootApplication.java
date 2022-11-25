35caf6300e7bdd6ec76cb4bdce3b52246d4f3d96
wyz <2947680284@qq.com>
2022-11-25 15:46:49 +0800

    first commit

---
+++ src/main/java/com/wyz/music_springboot/MusicSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wyz.music_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyz/music_springboot/MusicSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyz.music_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
